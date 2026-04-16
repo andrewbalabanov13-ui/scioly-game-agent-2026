213d7e80207a507e8fa8bae907c32f1a76d71b1e
Andrew Balabanov <andrewbalabanov13@gmail.com>
2026-04-15 18:51:33 -0700

    made the ai faster

--- godot/ai2.gd
+++ godot/ai2.gd
@@ -3,28 +3,33 @@ extends CharacterBody2D
 var play = false
 var q_table = []
 var world = []
-
+var last_state = 0
+var last_action = -1
+var action = -1
+var episode_amount = 0
+var robot_training_mode = -1
 const SPEED = 300.0
 const JUMP_VELOCITY = -400
 const TILEMULTIPLIER = 1
 const TILE_SIZE = 18
 const EPSILON_VALUE = 0.05
 const ALTHA = 0.1
+## Caps integration step for train/move. Without this, large Engine.time_scale makes delta huge and physics explodes.
+const MAX_PHYSICS_SUBSTEP := 1.0 / 120.0
 @export var tile: TileMapLayer
 
 func _ready() -> void:
 	pass
-	Engine.time_scale = 4
 	
 func get_reward(row,col):
 	var block = world[row][col]
 	if is_on_wall():
-		return -1000	
+		return Vector2i(-1000,1)
 	if block == "s":
-		return -100
+		return Vector2i(-100,2)
 	if block == "c":
-		return 20
-	return -1
+		return Vector2i(100,3)
+	return Vector2i(-1,1)
 
 func make_world():
 	var world = []
@@ -65,8 +70,12 @@ func get_row_and_col():
 	return Vector2i(row,col)
 
 func get_state(row,col):
-	return row*(tile.width_in_tiles+1) + col
+	return row* tile.width_in_tiles + col
 
+func reset():
+	position.x = 208
+	position.y = 120
+	
 func move(delta,dir):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -83,58 +92,91 @@ func move(delta,dir):
 		velocity.x = -1 * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	#print("b",+position.x)
-	#print(velocity.y)
-	#print("b",+ position.y)
 	move_and_slide()
-	#print(position.x)
-	#print(position.y)
-
-func episode(posx,posy):
-	position.x = posx
-	position.y = posy
 
 func train(posx,posy,delta):
+
 	var pos = get_row_and_col()
 	var row = pos.x
 	var col = pos.y
 	var state = get_state(row,col)
-	var action = -1
 	var index = -1
 	var b = -99999999999999
 	#print(state)
-	if randf() < EPSILON_VALUE:
-		action = randi_range(0,2)
-		#chooses a random number between 0-2
-		#possible choises include: 0,1,2
-		#number tells the direction of ai
-		#0 = right
-		#1 = left
-		#2 = up
-	else:
-		for x in range(len(q_table[state])):
-			if q_table[state][x] > b:
-				b = q_table[state][x]
-				index = x
-		action = index
+	
+	if state != last_state:
+		if last_state >= 0:
+			var reward_and_stop = get_reward(row,col)
+			var reward = reward_and_stop.x
+			var stop = reward_and_stop.y
+			var new_actions = q_table[state]
+			var TD = (reward + new_actions.max()) - q_table[last_state][action]
+			q_table[last_state][action] = q_table[last_state][action] + (ALTHA * TD)
+			if stop in [2,3]:
+				#if stop == 2:
+					#print('you lose')
+				#elif stop == 3:
+					#print('you win')
+				reset()
+				episode_amount += 1
+				return 
+		
+		if randf() < EPSILON_VALUE:
+			action = randi_range(0,2)
+			#chooses a random number between 0-2
+			#possible choises include: 0,1,2
+			#number tells the direction of ai
+			#0 = right
+			#1 = left
+			#2 = up
+		else:
+			for x in range(len(q_table[state])):
+				if q_table[state][x] > b:
+					b = q_table[state][x]
+					index = x
+			action = index
+		last_state = state
+	
 	move(delta,action)
 	pos = get_row_and_col()
 	row = pos.x
 	col = pos.y
-	var reward = get_reward(row,col)
-	var new_actions = q_table[get_state(row,col)]
-	var TD = (reward + new_actions.max()) - q_table[state][action]
-	q_table[state][action] = q_table[state][action] + (ALTHA * TD)
-
+	if is_on_wall():
+		if randf() < EPSILON_VALUE:
+			action = randi_range(0,2)
+			#chooses a random number between 0-2
+			#possible choises include: 0,1,2
+			#number tells the direction of ai
+			#0 = right
+			#1 = left
+			#2 = up
+		else:
+			for x in range(len(q_table[state])):
+				if q_table[state][x] > b:
+					b = q_table[state][x]
+					index = x
+			action = index
+		
 func _physics_process(delta: float) -> void:
 	if not play:
 		return
-	train(position.x,position.y,delta)
-
+	if robot_training_mode == 0:
+		if episode_amount < 1000:
+			Engine.time_scale = 1000
+			var steps: int = maxi(1, ceili(delta / MAX_PHYSICS_SUBSTEP))
+			var sub_delta: float = delta / float(steps)
+			for _i in range(steps):
+				train(position.x, position.y, sub_delta)
+		else:
+			Engine.time_scale = 1
+			print("done")
+	if robot_training_mode == 1:
+		pass
 
 func _on_main_menu_reset(type: Variant) -> void:
 	if type == "ai":
 		play = true
+		robot_training_mode = 0
 		position.x = 208
 		position.y = 120
 		world = make_world()
